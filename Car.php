dbeb061fd02afa0ff0118e6253d4eb4ae2ce2fcc
Nic Netzel and Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-02-08 11:43:37 -0800

    add encapsulation

--- Car.php
+++ Car.php
@@ -1,42 +1,73 @@
 <?php
   class Car
   {
-      public $make_model;
-      public $price;
-      public $miles;
+      private $make_model;
+      private $price;
+      private $miles;
+      private $accident_history;
+
+      function __construct ($car_type, $car_price, $car_miles, $accidents = 0)
+      {
+        $this->make_model = $car_type;
+        $this->price = $car_price;
+        $this->miles = $car_miles;
+        $this->accident_history = $accidents;
+      }
+      function setPrice($new_price)
+      {
+        $price = $new_price;
+      }
+      function getPrice()
+      {
+        return $this->price;
+      }
+
+      function setModel($new_make_model)
+      {
+        $this->make_model = $new_make_model;
+      }
+      function getModel()
+      {
+        return $this->make_model;
+      }
+
+      function setMiles($new_miles)
+      {
+        $this->miles = $new_miles;
+      }
+      function getMiles()
+      {
+        return $this->miles;
+      }
+
+      function setHistory ($new_history)
+      {
+        $this->accident_history = $new_history;
+      }
+      function getHistory()
+      {
+        return $this->accident_history;
+      }
+
 
       function worthBuying($max_price)
       {
         return $this->price < ($max_price +100);
       }
+
   }
 
-  $porsche = new Car();
-  $porsche->make_model = "2014 Porsche 911";
-  $porsche->price = 114991;
-  $porsche->miles = 7864;
-
-  $ford = new Car();
-  $ford->make_model = "2011 Ford F450";
-  $ford->price = 55995;
-  $ford->miles = 14211;
-
-  $lexus = new Car();
-  $lexus->make_model = "2013 Lexus RX 350";
-  $lexus->price = 44700;
-  $lexus->miles = 20000;
-
-  $mercedes = new Car();
-  $mercedes->make_model = "Mercedes Benz CL550";
-  $mercedes->price = 39900;
-  $mercedes->miles = 37979;
+  $porsche = new Car("2014 Porsche 911", 114991, 7864, 2);
+  $ford = new Car("2011 Ford F450", 55995, 14211);
+  $lexus = new Car("2013 Lexus RX 350", 44700, 20000,9);
+  $mercedes = new Car("Mercedes Benz CL550", 39900, 37979);
 
   $cars = array($porsche, $ford, $lexus, $mercedes);
-$cars_matching_search = array();
-foreach ($cars as $car){
-  if ($car->worthBuying($_GET['price'])){
+  $cars_matching_search = array();
+      foreach ($cars as $car){
+          if ($car->worthBuying($_GET['price'])){
 
-    array_push($cars_matching_search, $car);
+      array_push($cars_matching_search, $car);
   }
 }
 ?>
@@ -52,10 +83,15 @@ foreach ($cars as $car){
       <ul>
           <?php
               foreach ($cars_matching_search as $car) {
-                  echo "<li> $car->make_model </li>";
+                 $show_model = $car->getModel();
+                 $show_price = $car->getPrice();
+                 $show_miles = $car->getMiles();
+                 $show_history = $car->getHistory();
+                  echo "<li> $show_model </li>";
                   echo "<ul>";
-                      echo "<li> $car->price </li>";
-                      echo "<li> Miles: $car->miles </li>";
+                      echo "<li> $$show_price </li>";
+                      echo "<li> Miles: $show_miles </li>";
+                      echo "<li> Number of Accidents: $show_history </li>";
                   echo "</ul>";
               }
           ?>
